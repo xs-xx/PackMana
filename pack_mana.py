20c9b85b6e34236d36268bcc33870da9dc048f5c
xs-xx <xses.cb@gmail.com>
2024-08-20 02:03:18 -0400

    Update pack_mana.py

--- pack_mana.py
+++ pack_mana.py
@@ -7,7 +7,7 @@ import os
 import inspect
 import importlib
 
-class Package_Mana:
+class Pack_Mana:
     def __init__(self):
         self.imported_packages = []
         self.script_path = self.get_caller_script_path()
